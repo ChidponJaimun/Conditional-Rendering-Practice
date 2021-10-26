4c4ef2225e365be0957e4c4bad5a6942a91f32b9
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-10-27 06:42:04 +0700

    55

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import Input from "./Input";
 
-function Form() {
+function Form(props) {
   return (
     <form className="form">
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      <Input type="username" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      {!props.registed && (
+        <Input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">{props.registed ? "Login" : "Register"}</button>
     </form>
   );
 }
